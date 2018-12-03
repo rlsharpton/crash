19757aaa4f5b01236efcec674b510ded1ba9d13e
rlsharpton <robin@robinls.net>
2018-12-03 09:38:00 -0600

    12/3/18 Too much time has passed.

---
+++ messageNew.py
@@ -0,0 +1,12 @@
+#! python3
+__author__ = 'rls'
+__version__ = 0.1
+''' Program written by Robin Sharpton
+    rest of DocString goes here 
+'''
+
+message = "Be Present. Be Patient. Be Here Now."
+print(message)
+print(message.lower())
+print(message.title())
+
