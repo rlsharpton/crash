467601cff84e83a9f3a2261c9ce7cdf084a39d5e
Robin Sharpton <robin@robinls.net>
2021-03-03 21:15:23 -0600

    WedMarch3 Code Kata

---
+++ march3codekata.py
@@ -0,0 +1,23 @@
+def sumofNum(n):
+    """Simple function to sum numbers from 1 to n """
+    num = 1
+    while num < n:
+        total = 0
+        total = total + num
+        print(f"total is: {total}")
+        num += 1
+        print(f"num is: {num}")
+
+    return total
+
+
+try:
+    addTo = input("Please enter an integer to add up to: ")
+    number = int(addTo)
+except ValueError:
+    print("You have not entered an integer")
+
+print(f"the sum of {number} is {sumofNum(number)}")
+
+
+    
